0ab3beb36a5f81084827001c4c0dc54c909e8f97
Theo1 <theo1laris@icloud.com>
2026-04-29 17:36:48 +0200

    Implementation de l'authentification avec l'entité User: enregistrement des email en BDD, condition generale en lien cliquable

--- app/src/Form/RegistrationFormType.php
+++ app/src/Form/RegistrationFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use RegexIterator;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -11,6 +12,8 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
 
 class RegistrationFormType extends AbstractType
 {
@@ -18,31 +21,33 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
+          ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'label' => false, 
                 'constraints' => [
-                    new IsTrue(
-                        message: 'You should agree to our terms.',
-                    ),
+                    new IsTrue([
+                        'message' => 'Vous devez accepter les conditions.',
+                    ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank(
-                        message: 'Please enter a password',
-                    ),
-                    new Length(
-                        min: 6,
-                        minMessage: 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        max: 4096,
-                    ),
-                ],
-            ])
+           ->add('plainPassword', PasswordType::class, [
+    'mapped' => false,
+    'attr' => ['autocomplete' => 'new-password'],
+    'constraints' => [
+        new NotBlank([
+            'message' => 'Veuillez entrer un mot de passe',
+        ]),
+        new Length([
+            'min' => 12,
+            'minMessage' => 'Le mot de passe doit contenir au moins 12 caractères',
+        ]),
+        new Regex([
+            'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).+$/',
+            'message' => 'Le mot de passe doit contenir une majuscule, une minuscule, un chiffre et un caractère spécial.',
+        ]),
+    ],
+])
+           
         ;
     }
 
