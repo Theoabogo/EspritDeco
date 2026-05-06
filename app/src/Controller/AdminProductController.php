b79a6b64bbaad66f1226db13da79f192a1b19ecc
Theo1 <theo1laris@icloud.com>
2026-05-06 09:48:58 +0200

    Mise à jour du controller et de la vue form.html.twig

--- app/src/Controller/AdminProductController.php
+++ app/src/Controller/AdminProductController.php
@@ -10,6 +10,7 @@ use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\ProductType;
 
 final class AdminProductController extends AbstractController
 {
@@ -22,7 +23,7 @@ final class AdminProductController extends AbstractController
     }
      #[Route('/admin/products/delete/{id}', name: 'admin_product_delete')]
      public function delete(Product $product, EntityManagerInterface $em): Response
-     {
+    {
         
      foreach ($product->getImages() as $image) {
          $filePath = $this->getParameter('kernel.project_dir') . '/public/' . $image->getPath();
@@ -36,5 +37,29 @@ final class AdminProductController extends AbstractController
                 $this->addFlash('success', 'Produit supprimé avec succès.');
 
          return $this->redirectToRoute('app_admin_product_list');
-     }
+    }
+
+    #[Route('/admin/products/add', name: 'admin_product_add')]
+    public function add( Request $request, EntityManagerInterface $em):Response
+    {
+            $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $product= $form->getData();            
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', "L'objet a été ajouté avec succès.");
+
+            return $this->redirectToRoute('app_admin_product_list');
+        }
+        
+        return $this->render('admin_product/form.html.twig', [
+            'form' => $form->createView()
+        ]);
+        
+        }
+
 }
